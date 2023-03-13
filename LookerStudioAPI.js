6f63165a5a6947f4236483f601509e3bade4486a
Darilson <57709084+Darilson@users.noreply.github.com>
2023-03-13 09:34:56 -0300

    Update LookerStudioAPI.js
    The version is working, but when changing values from another column it is also skipping the row. It is close to the correct functionality

--- LookerStudioAPI.js
+++ LookerStudioAPI.js
@@ -1,24 +1,84 @@
 function importCSVFromGoogleDrive() {
-  var csvFile = DriveApp.getFilesByName("PolymerDispFrequency0.csv").next();
-  var csvData = Utilities.parseCsv(csvFile.getBlob().getDataAsString('ISO-8859-1'));
-  var bufferSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("BufferSheet0");
-  if (!bufferSheet) {
-    bufferSheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("BufferSheet0");
-  }
-  bufferSheet.clearContents();
-  bufferSheet.getRange(1, 1, csvData.length, csvData[0].length).setValues(csvData);
+  const fileName = "PolymerDispFrequency0.csv";
+  const csvFile = DriveApp.getFilesByName(fileName).next();
+  const csvData = Utilities.parseCsv(csvFile.getBlob().getDataAsString('ISO-8859-1'));
 
-  var targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("PolymerDispFrequency0");
+  const bufferSheetName = "BufferSheet0";
+  const bufferSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(bufferSheetName) || SpreadsheetApp.getActiveSpreadsheet().insertSheet(bufferSheetName);
+  bufferSheet.clear();
+
+  const bufferRange = bufferSheet.getRange(1, 1, csvData.length, csvData[0].length);
+  bufferRange.setValues(csvData);
+
+  const targetSheetName = "PolymerDispFrequency0";
+  const targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(targetSheetName);
   if (!targetSheet) {
-    throw new Error("Sheet not found: PolymerDispFrequency0");
+    throw new Error(`Sheet not found: ${targetSheetName}`);
   }
 
-  var targetLastRow = targetSheet.getLastRow();
-  var bufferLastRow = bufferSheet.getLastRow();
-  if (bufferLastRow > targetLastRow) {
-    var newRows = bufferSheet.getRange(targetLastRow + 1, 1, bufferLastRow - targetLastRow, csvData[0].length).getValues();
-    targetSheet.getRange(targetLastRow + 1, 1, newRows.length, newRows[0].length).setValues(newRows);
+  const validityColumnIndex = getColumnIndexByHeader(targetSheet, "Validity");
+  const targetData = getSheetData(targetSheet);
+  const bufferData = getSheetData(bufferSheet);
+
+  const newRows = getNewRows(bufferData, targetData, validityColumnIndex);
+
+  if (newRows.length > 0) {
+    appendRows(targetSheet, newRows);
   }
 
-  SpreadsheetApp.getActiveSpreadsheet().deleteSheet(bufferSheet);
+  deleteSheet(bufferSheet);
+}
+
+function getSheet(name) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(name);
+  if (!sheet) {
+    throw new Error(`Sheet not found: ${name}`);
+  }
+  return sheet;
+}
+
+function getColumnIndexByHeader(sheet, header) {
+  const headerRow = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  return headerRow.findIndex(value => value === header);
+}
+
+function getSheetData(sheet) {
+  const range = sheet.getDataRange();
+  const values = range.getValues();
+  const headerRowIndex = 0;
+  return Array.from(values, (row, rowIndex) => {
+    if (rowIndex === headerRowIndex) {
+      return row;
+    }
+    return row.filter(value => value !== "");
+  });
+}
+
+function getNewRows(bufferData, targetData, validityColumnIndex) {
+  const uniqueIds = targetData.reduce((ids, row) => {
+    if (row[validityColumnIndex] !== 100) {
+      ids[row.join("|")] = true;
+    }
+    return ids;
+  }, {});
+
+  const newRows = bufferData.reduce((rows, row) => {
+    if (row[validityColumnIndex] !== 100 && !uniqueIds[row.join("|")]) {
+      rows.push(row);
+      uniqueIds[row.join("|")] = true;
+    }
+    return rows;
+  }, []);
+
+  return newRows;
+}
+
+function appendRows(sheet, rows) {
+  const startRow = sheet.getDataRange().getNumRows() + 1;
+  const range = sheet.getRange(startRow, 1, rows.length, rows[0].length);
+  range.setValues(rows);
+}
+
+function deleteSheet(sheet) {
+  SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
 }
